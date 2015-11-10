da86dee842475d3877a0fe8781e1368b4daab246
MariCat <fjkfwz@hotmail.com>
2015-11-10 15:30:12 +0800

    add Binary Tree

--- src/main/java/Tree/BinaryTree/BinaryTree.java
+++ src/main/java/Tree/BinaryTree/BinaryTree.java
@@ -62,13 +62,12 @@ public class BinaryTree {
      * @return
      */
     public List<Integer> preorderTraversal(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return list;
+        }
         list.add(node.getVal());
-        if (node.getLeft() != null) {
-            preorderTraversal(node.getLeft(), list);
-        }
-        if (node.getRight() != null) {
-            preorderTraversal(node.getRight(), list);
-        }
+        preorderTraversal(node.getLeft(), list);
+        preorderTraversal(node.getRight(), list);
         return list;
     }
 
@@ -80,13 +79,12 @@ public class BinaryTree {
      * @return
      */
     public List<Integer> inorderTraversal(TreeNode node, List<Integer> list) {
-        if (node.getLeft() != null) {
-            inorderTraversal(node.getLeft(), list);
+        if (node == null) {
+            return list;
         }
+        inorderTraversal(node.getLeft(), list);
         list.add(node.getVal());
-        if (node.getRight() != null) {
-            inorderTraversal(node.getRight(), list);
-        }
+        inorderTraversal(node.getRight(), list);
         return list;
     }
 
@@ -98,83 +96,112 @@ public class BinaryTree {
      * @return
      */
     public List<Integer> postorderTraversal(TreeNode node, List<Integer> list) {
-        if (node.getLeft() != null) {
-            postorderTraversal(node.getLeft(), list);
+        if (node == null) {
+            return list;
         }
-        if (node.getRight() != null) {
-            postorderTraversal(node.getRight(), list);
-        }
+        postorderTraversal(node.getLeft(), list);
+        postorderTraversal(node.getRight(), list);
         list.add(node.getVal());
         return list;
     }
 
+    /**
+     * Judge A Binary Tree whether is a BST;
+     *
+     * @param node
+     * @param isVaild
+     * @return
+     */
     public boolean isValidBST(TreeNode node, boolean isVaild) {
+        if (node == null) {
+            return isVaild;
+        }
         if ((node.getLeft() != null && node.getLeft().getVal() > node.getVal()) || (node.getRight() != null && node.getRight().getVal() < node.getVal())) {
             isVaild = false;
         }
-        if (node.getLeft() != null) {
-            isVaild = isValidBST(node.getLeft(), isVaild);
-        }
-        if (node.getRight() != null) {
-            isVaild = isValidBST(node.getRight(), isVaild);
-        }
+        isVaild = isValidBST(node.getLeft(), isVaild);
+        isVaild = isValidBST(node.getRight(), isVaild);
         return isVaild;
     }
 
+    /**
+     * Find BST mini Depth
+     *
+     * @param node
+     * @param count
+     * @return
+     */
     public int minDepth(TreeNode node, int count) {
+        if (node == null) {
+            return count;
+        }
         count++;
         int rightcount = count;
         int leftcount = count;
         if (node.getLeft() == null || node.getLeft() == null) {
             return count;
         }
-        if (node.getLeft() != null) {
-            leftcount = minDepth(node.getLeft(), count);
-        }
-        if (node.getRight() != null) {
-            rightcount = minDepth(node.getRight(), count);
-        }
+        leftcount = minDepth(node.getLeft(), count);
+        rightcount = minDepth(node.getRight(), count);
         return Math.min(leftcount, rightcount);
     }
 
+    /**
+     * Find BST max Depth
+     *
+     * @param node
+     * @param count
+     * @return
+     */
     public int maxDepth(TreeNode node, int count) {
+        if (node == null) {
+            return count;
+        }
         count++;
         int rightcount = count;
         int leftcount = count;
-        if (node.getLeft() != null) {
-            leftcount = maxDepth(node.getLeft(), count);
-        }
-        if (node.getRight() != null) {
-            rightcount = maxDepth(node.getRight(), count);
-        }
+        leftcount = maxDepth(node.getLeft(), count);
+        rightcount = maxDepth(node.getRight(), count);
         return Math.max(leftcount, rightcount);
     }
 
+    /**
+     * Invert Binary Tree
+     *
+     * @param node
+     */
     public void invertBinaryTree(TreeNode node) {
-            TreeNode tmpTree = node.getLeft();
-            node.setLeft(node.getRight());
-            node.setRight(tmpTree);
-        if (node.getLeft() != null) {
-            invertBinaryTree(node.getLeft());
+        if (node == null) {
+            return;
         }
-        if (node.getRight() != null) {
-            invertBinaryTree(node.getRight());
-        }
+        TreeNode tmpTree = node.getLeft();
+        node.setLeft(node.getRight());
+        node.setRight(tmpTree);
+        invertBinaryTree(node.getLeft());
+        invertBinaryTree(node.getRight());
     }
 
-    public TreeNode flatten(TreeNode node) {
-        TreeNode LeftNode = node;
-        TreeNode RightNode = node;
-        if (node.getLeft() != null) {
-             LeftNode= flatten(node.getLeft());
+    /**
+     * Flatten Binary Tree to Linked List
+     * Flatten a binary tree to a fake "linked list" in pre-order traversal.
+     * Here we use the right pointer in TreeNode as the next pointer in ListNode.
+     *
+     * @param node
+     * @param lastNode
+     */
+    public void flatten(TreeNode node, TreeNode lastNode) {
+        if (node == null) {
+            return;
         }
-
-        if (node.getRight() != null) {
-            RightNode = flatten(node.getRight());
+        if (lastNode != null) {
+            lastNode.setLeft(null);
+            lastNode.setRight(node);
         }
-        node.setRight(LeftNode);
-        node.getRight().setRight(RightNode);
-        return node;
+        lastNode = node;
+        flatten(node.getLeft(), lastNode);
+        flatten(node.getRight(), lastNode);
     }
+
+
 }
 
